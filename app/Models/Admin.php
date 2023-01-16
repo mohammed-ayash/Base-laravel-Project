5af141256bcd6ec139344a58d571d0070641f641
Mohammed Ayash <mohammed.ayash@sheenvalue.com>
2023-01-16 23:15:41 +0300

    add role and permission

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -6,12 +6,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class Admin extends Authenticatable
 {
     use HasApiTokens;
     use HasFactory;
     use Notifiable;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
